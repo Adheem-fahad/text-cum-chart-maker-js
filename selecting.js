b76c9727672bc8b08b4dc547f153a790372dbc02
ME <kuflesam@gmail.com>
2024-03-11 12:05:06 +0400

    ahhhh ahh ah ahh! this roads of---

--- selecting.js
+++ selecting.js
@@ -3,43 +3,43 @@ import { el, css } from './utilities.js'
 export default class MaketempDiv {
     constructor(x, y) {
         this.father = x
-        this.onstart(y)
+        this.onstart()
+        this.y = y
     }
-    exportFn(y) {
+    exportFn(y = this.y) {
         // console.log(this.ok)
-        if(this.ok == 200 && this.temp) {
+        // if(this.ok == 200 && this.temp) {
             // console.log('reached.... station exportFn')
 
-            document.removeEventListener('mousemove', this.dragfn)
+            // document.removeEventListener('mousemove', this.dragfn)
+
+            document.onmousemove = null
     
             this.exportedDiv = this.temp
         
-            y(this.exportedDiv)
-        } else if(this.temp) { 
-            document.removeEventListener('mousemove', this.dragfn)
+            // y(this.exportedDiv)
+        // } else if(this.temp) { 
+            // document.removeEventListener('mousemove', this.dragfn)
 
             this.temp.parentElement.removeChild(this.temp)
             this.temp = ''
-        }
+        // }
     }
-    dragfn(e, y) {
+    dragfn(e) {
 
-        this.ok = 200
-
-        if(!this.temp) return
         css(this.temp, {
             width: `${e.clientX - this.initPos[0]}px`,
             height: `${e.clientY - this.initPos[1]}px`
         })
 
-        document.addEventListener('mouseup', () => {
-            this.exportFn(y)
-        })
+        // document.addEventListener('mouseup', this.exportFn())
+        document.onmouseup = (ev) => {
+            this.exportFn()
+        }
     }
     onstart(y) {
         this.father.addEventListener('mousedown', (e) => {
 
-            this.ok = 0
             this.temp = el('div', this.father, ['class', 'tempo'])
             this.initPos = [e.clientX, e.clientY]
             css(this.temp, {
@@ -47,7 +47,9 @@ export default class MaketempDiv {
                 top: `${this.initPos[1] + this.father.parentElement.scrollTop}px`
             })
 
-            document.addEventListener('mousemove', (ev) => { this.dragfn(ev, y) }, false)
+            document.onmousemove = (ev) => {
+                this.dragfn(ev)
+            }
         }, false);
     }
 }
--- stickyNote.js
+++ stickyNote.js
@@ -1,93 +1,90 @@
-import { el, css } from './utilities.js'
-import Creator from './inputTake.js'
+// import { el, css } from './utilities.js'
+// import Creator from './inputTake.js'
 
-export class StickyNote {
-    constructor(element) {
-        this.father = element;
-        this.creaiososis = new Creator(this.father)
-        // this.headerBr = el('div', this.father, ['class', 'hbr'])
+// export class StickyNote {
+//     constructor(element) {
+//         this.father = element;
+//         this.creaiososis = new Creator(this.father)
+//         // this.headerBr = el('div', this.father, ['class', 'hbr'])
 
-        // this.addingEditCont()
+//         // this.addingEditCont()
         
-        this.father.addEventListener('mousedown', (e) => {
-            this.mousetouch(e)
-        }) 
+//         this.father.addEventListener('mousedown', (e) => {
+//             this.mousetouch(e)
+//         }) 
 
-        this.scaffolding()
-    }
+//         this.scaffolding()
+//     }
 
-    addingEditCont() {
-        this.textCnt = el('h3', this.father, ['contentEditable', 'true'])
-        // el('span', this.textCnt)
-        this.textCnt.focus()
-        this.textCnt.onclick = () => {
-            document.addEventListener('keydown', () => {
-                this.lclStredtxt = this.textCnt.textContent
-            })
-        }
-    }
-
-
-    scafdrag(e) {
-        // document.addEventListener('mousemove', () => {/
-        // })
-    }
-    scaffolding() {
-        const allCircs = [] 
-        let allPos = [
-            {'left': `${0 - 6}px`, 'top': `${0 - 6}px`},
-            {'right': `${0 - 6}px`, 'top': `${0 - 6}px`},
-            {'left': `${0 - 6}px`, 'bottom': `${0 - 6}px`},
-            {'right': `${0 - 6}px`, 'bottom': `${0 - 6}px`}
-        ]
+//     addingEditCont() {
+//         this.textCnt = el('h3', this.father, ['contentEditable', 'true'])
+//         // el('span', this.textCnt)
+//         this.textCnt.focus()
+//         this.textCnt.onclick = () => {
+//             document.addEventListener('keydown', () => {
+//                 this.lclStredtxt = this.textCnt.textContent
+//             })
+//         }
+//     }
 
 
-        for(let i = 0; i < 4; i++) {
-            allCircs.push(el('div', this.father, ['class', 'piCl']))
+//     scafdrag(e) {
+//         // document.addEventListener('mousemove', () => {/
+//         // })
+//     }
+//     scaffolding() {
+//         const allCircs = [] 
+//         let allPos = [
+//             {'left': `${0 - 6}px`, 'top': `${0 - 6}px`},
+//             {'right': `${0 - 6}px`, 'top': `${0 - 6}px`},
+//             {'left': `${0 - 6}px`, 'bottom': `${0 - 6}px`},
+//             {'right': `${0 - 6}px`, 'bottom': `${0 - 6}px`}
+//         ]
 
-            // console.log(allPos[i])
-            css(allCircs[i], allPos[i])
 
-            allCircs.forEach(element => {
-                addEventListener('mousedown', (e) => {
-                    this.scafdrag(e)
+//         for(let i = 0; i < 4; i++) {
+//             allCircs.push(el('div', this.father, ['class', 'piCl']))
 
-                    // console.log('hey')
-                })
-            })
-        }
-    }
+//             // console.log(allPos[i])
+//             css(allCircs[i], allPos[i])
 
-    closeDrag() {
-        // document.removeEventListener('mousemove', this.draggable)
-        document.onmousemove = null
+//             // allCircs.forEach(element => {
+//             //     addEventListener('mousedown', (e) => {
+//             //         this.scafdrag(e)
 
-        this.father.style.border = '2px solid rgba(92, 92, 255, 0.358)'
-    }
-    draggable(num1, num2, e) {
-            // console.log(e.offsetY, e.offsetX)
-            console.log('moving')
+//             //         // console.log('hey')
+//             //     })
+//             // })
+//         }
+//     }
 
-            const DOC_TSKw = document.querySelector('.tskbr').clientWidth
+//     closeDrag() {
+//         // document.removeEventListener('mousemove', this.draggable)
+//         document.onmousemove = null
 
-            css(this.father, {
-                top: `${e.clientY + this.father.parentElement.scrollTop - num2}px`,
-                left: `${e.clientX - DOC_TSKw - num1}px`
-            })
+//         this.father.style.border = '2px solid rgba(92, 92, 255, 0.358)'
+//     }
+//     draggable(num1, num2, e) {
+//             // console.log(e.offsetY, e.offsetX)
+//             console.log('moving')
 
-            document.addEventListener('mouseup', (e) => {
-                console.log('yo shit.. its up')
-                e.preventDefault()
+//             const DOC_TSKw = document.querySelector('.tskbr').clientWidth
 
-                this.closeDrag()
-            })
-    }
-    mousetouch(e) {
-                // document.addEventListener('mousemove', (e) => { this.draggable(e.offsetX, e.offsetY, e) })
-                document.onmousemove = (e) => {
-                    this.draggable(e.offsetX, e.offsetY, e)
-                }
+//             css(this.father, {
+//                 top: `${e.clientY + this.father.parentElement.scrollTop - num2}px`,
+//                 left: `${e.clientX - DOC_TSKw - num1}px`
+//             })
 
-                this.father.style.border = '2px solid rgba(177, 177, 255, 0.542)'
-    }
-}
+//             document.addEventListener('mouseup', (e) => {
+//                 console.log('yo shit.. its up')
+//                 e.preventDefault()
+
+//                 this.closeDrag()
+//             })
+//     }
+//     mousetouch(e) {
+//                 document.addEventListener('mousemove', this.draggable(e.offsetX, e.offsetY, e))
+
+//                 this.father.style.border = '2px solid rgba(177, 177, 255, 0.542)'
+//     }
+// }
